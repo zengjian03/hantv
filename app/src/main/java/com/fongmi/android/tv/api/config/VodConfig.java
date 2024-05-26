7263319e05ea4a3537f6db3fa604d2ee8c0b502f
zengjian0303 <zengjian0303@126.com>
2024-05-26 17:53:16 +0800

    jian

--- app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
@@ -1,6 +1,7 @@
 package com.fongmi.android.tv.api.config;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.fongmi.android.tv.App;
 import com.fongmi.android.tv.R;
@@ -136,13 +137,13 @@ public class VodConfig extends Updater {
 
     private void loadConfig(Callback callback) {
         try {
+            //jian
             String url = config.getUrl();
-            //Log.i("zengjian","==" +url + "---");
             if (TextUtils.isEmpty(url)) {
                 url = Hawk.get(HawkConfig.API_URL);
-                // 添加以下代码，解决内置源时，投屏播放问题，给定一个配置，写入本地数据库，标记一个name（名字“源已内置”可以随便取，但一定要有，type为0,表示点播）
                 Config.find(url, 0).name("源已内置").update();
             }
+            //jian
             checkJson(Json.parse(Decoder.getJson(config.getUrl())).getAsJsonObject(), callback);
         } catch (Throwable e) {
             if (TextUtils.isEmpty(config.getUrl())) App.post(() -> callback.error(""));
